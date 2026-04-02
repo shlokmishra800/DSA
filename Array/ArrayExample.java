eacc86bb93b5b3134db50ef9c305a84d1051f408
Shlok Mishra <shlokmishra576@gmail.com>
2026-04-02 19:37:37 +0530

    added Array folder with definition and example

---
+++ Array/ArrayExample.java
@@ -0,0 +1,16 @@
+
+
+package Array;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int [] marks = new int[3];        
+        marks[0] = 56;
+        marks[1]= 58;
+        marks[2] = 86;
+        for(int i = 0;i<3 ; i++){
+            System.out.println(marks[i]);
+        } 
+
+    }
+}
