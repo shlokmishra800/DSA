f8b2bfbe8bc411384d10c971ad5e88f1acbeb636
Shlok Mishra <shlokmishra576@gmail.com>
2026-04-04 11:40:52 +0530

    added string folder with definition and example

---
+++ String/PrintName.java
@@ -0,0 +1,10 @@
+package String;
+import java.util.*;
+public class PrintName {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name = sc.next();
+        System.out.println("hello " +name +" how are you ?" );
+    }
+}
